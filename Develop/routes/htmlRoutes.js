8fbcf26051433e1689c9b463960875c4f7355e1e
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-09-05 04:03:16 -0700

    added second content

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const { dirname } = require("path");
 const path = require("path");
 const router = express.Router();
+
+// ROUTES
+
+router.get("/",(req,res) => (
+    res.sendFile(path.join(dirname, "../public/index.html"))
+));
