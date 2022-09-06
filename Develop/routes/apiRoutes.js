708e7766491565d960977e13b5839da658939f8a
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-09-05 21:07:36 -0700

    began code, added consts

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const filePath = path.join(__dirname, '../db/db.json');
+
+// Get request
+
+
+// Post Request
+
