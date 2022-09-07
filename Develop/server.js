52fdc7e19951dfb8b2ca9762dead79daa200a59b
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-09-06 21:27:01 -0700

    added middleware content

--- Develop/server.js
+++ Develop/server.js
@@ -4,3 +4,10 @@ const fs = require('fs');
 const app = express();
 
 var PORT = process.env.PORT || 3000;
+
+// MIDDLEWARE
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
