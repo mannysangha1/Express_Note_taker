2d3d20446b7ea018ef3142c061cf56d55cd3f48f
Manny Sangha <mannysangha@Mannys-MacBook-Pro.local>
2022-09-06 23:37:01 -0700

    fixed minor error

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const { dirname } = require("path");
 const path = require("path");
 const router = express.Router();
 
 // ROUTES
 
 router.get("/",(req,res) => (
-    res.sendFile(path.join(dirname, "../public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 ));
 
 router.get("/notes",(req,res) =>(
-    res.sendFile(path.join(dirname, "../public/notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
 ));
 
 module.exports = router;
