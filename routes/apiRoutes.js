ded7bad36db83a20bcb0f21c5272fd76456667b2
Manny <mannysangha020@gmail.com>
2022-09-23 15:21:18 -0700

    test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -22,20 +22,37 @@ router.post("/api/notes", async (req, res)=>{
     })
 })
 
-router.delete("/api/notes/:id", async (req, res) => {
-    newArr = []
-    console.log(req.params.id);
-    let id = req.params.id;
-    fs.readFile(filePath, function (err, data) {
-        let json = JSON.parse(data)
-        console.log(json);
-        newArr = json.filter(json=>json.id!==id)
-        console.log(newArr);
-        newArr = JSON.stringify(newArr);
-        fs.writeFileSync(path.join(__dirname, '../db/db.json'),newArr);
-        res.sendFile(path.join(__dirname, '../db/db.json'))
-    })
-})
+router.delete("/api/notes/:id", async (req, res) =>{
+   newArr = []
+   console.log(req.params.id);
+   let id = req.params.id;
+   fs.readFile(filePath, function (err, data){
+       let json = JSON.parse(data)
+       console.log(json);
+       newArr = json.filter(json=>json.id!==id)
+       console.log(newArr);
+       newArr = JSON.stringify(newArr);
+       fs.writeFileSync(path.join(__dirname, '../db/db.json'),newArr);
+       res.sendFile(path.join(__dirname, '../db/db.json'))
+        })
+   })
+    
+
+
+    
+
+
+
+        //fs.readFile(filePath, function (err, data) {
+       // let json = JSON.parse(data)
+        //console.log(json);
+        //newArr = json.filter(json=>json.id!==id)
+        //console.log(newArr);
+        //newArr = JSON.stringify(newArr);
+        //fs.writeFileSync(path.join(__dirname, '../db/db.json'),newArr);
+        //res.sendFile(path.join(__dirname, '../db/db.json'))
+   // })
+//})
 
 
 
